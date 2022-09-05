5bce0cf1e95577f5c9b8bddd9385faf8255a0eda
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2022-09-05 11:29:33 +0800

    Creating Models, Migrations, Model Relations
    protected $fillable, $this->hasmany, belongsto, change login attempt to 3, 350 seconds time, install filament. In migration file, did foreignID,string,char and can do constrained -> cascadeondelete

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,8 +45,8 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
+        if (!Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey(), 350);
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
@@ -65,7 +65,7 @@ class LoginRequest extends FormRequest
      */
     public function ensureIsNotRateLimited()
     {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
             return;
         }
 
@@ -88,6 +88,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey()
     {
-        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->input('email')) . '|' . $this->ip());
     }
 }
---
+++ app/Models/Employee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['first_name',
+    'last_name',
+    'address',
+    'city_id',
+    'state_id',
+    'department_id',
+    'country_id',
+    'zip_code',
+    'birth_date',
+    'date_hired'];
+
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function state(){
+        return $this->belongsTo(States::class);
+    }
+
+    public function city(){
+        return $this->belongsTo(cities::class);
+    }
+    
+}
